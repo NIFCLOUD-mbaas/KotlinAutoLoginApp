e3a330841f428237af494bbb79e0961a9848a678
Jimmy Huynh <vfa.tamhh@gmail.com>
2022-08-31 09:44:50 +0700

    Change NCMB SDK 1.2.0  Target api 33  upgrade android gradle  update android studio environment

--- README.md
+++ README.md
@@ -16,10 +16,10 @@
 
 ## 動作環境
 
-* Android Studio ver. 3.1
-* Android OS ver. 6.0, OS ver. 4.4.2
-* Android SDK v3
-  - SDK v2系だと動作しないので注意
+* MacOS Monterey version 12.5
+* Android Studio Chipmunk | 2021.2.1 Patch 2
+* Pixel 2 - Android 13 (Simulator)
+* KotlinSDK v1.2.0
 
 ※上記内容で動作確認をしています。
 
@@ -53,10 +53,10 @@
 
 ### 3. SDKの導入（実装済み）
 
-※このサンプルアプリには既にSDKが実装済み（下記手順）となっています。（ver.3.0.0)<br>　最新版をご利用の場合は入れ替えてご利用ください。
+※このサンプルアプリには既にSDKが実装済み（下記手順）となっています。（ver.1.2.0)<br>　最新版をご利用の場合は入れ替えてご利用ください。
 
 * SDKダウンロード
-SDKはここ（[SDK リリースページ](https://github.com/NIFCloud-mbaas/ncmb_android/releases)）から取得してください.
+SDKはここ（[SDK リリースページ](https://github.com/NIFCLOUD-mbaas/ncmb_kotlin/releases)）から取得してください.
   - NCMB.jarファイルがダウンロードします。
 * SDKをインポート
   - app/libsフォルダにNCMB.jarをコピーします
@@ -64,8 +64,12 @@ SDKはここ（[SDK リリースページ](https://github.com/NIFCloud-mbaas/ncm
   - app/build.gradleファイルに以下を追加します
 ```gradle
 dependencies {
-    compile 'com.google.code.gson:gson:2.3.1'
-    compile files('libs/NCMB.jar')
+    implementation 'com.squareup.okhttp3:okhttp:4.8.1'
+    implementation 'com.google.code.gson:gson:2.3.1'
+    api files('libs/NCMB.jar')
+
+    //同期処理を使用する場合はこちらを追加していただく必要があります
+    implementation 'org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9'
 }
 ```
   - androidManifestの設定
@@ -147,8 +151,10 @@ dependencies {
 #### 自動ログイン処理
 ```kotlin
 // UUIDを取得します
-uuid = Secure.getString(applicationContext.contentResolver,
-        Secure.ANDROID_ID)
+uuid = Secure.getString(
+    applicationContext.contentResolver,
+    Secure.ANDROID_ID
+)
 Log.d(TAG, uuid)
 
 setContentView(R.layout.activity_main)
@@ -157,59 +163,57 @@ _txtLogin = findViewById<TextView>(R.id.txtLogin)
 
 //Login
 try {
-    NCMBUser.loginInBackground(uuid, uuid) { login_user, e ->
+    var user = NCMBUser()
+    user.loginInBackground(uuid, uuid, NCMBCallback { e, login_user ->
         if (e != null) {
             //エラー時の処理
             Log.d(TAG, e.code)
             if (e.code == "E401002") {
-
                 //NCMBUserのインスタンスを作成
-                val user = NCMBUser()
-                //ユーザ名を設定
-                user.userName = uuid
-                //パスワードを設定
-                user.setPassword(uuid)
-                //設定したユーザ名とパスワードで会員登録を行う
-                user.signUpInBackground { er ->
-                    if (er != null) {
-                        //会員登録時にエラーが発生した場合の処理
-                        Log.d(TAG, "Signup error$er")
-                    } else {
-                        _txtMessage.text = "はじめまして"
-                        _txtLogin.text = "１回目ログイン、ありがとうございます。"
-                        //lastLoginを更新します（ラストログインのタイミングを取得するために）
-                        try {
-                            val curUser = NCMBUser.getCurrentUser()
-                            val now = Date()
-                            curUser.put("lastLoginDate", now)
-                            curUser.save()
-                        } catch (e1: NCMBException) {
-                            e1.printStackTrace()
-                        }
+                val signupUser = NCMBUser()
+                signupUser.signUpInBackground(uuid, uuid, NCMBCallback { er, signUpUser ->
+                    runOnUiThread {
+                        if (er != null) {
+                            //会員登録時にエラーが発生した場合の処理
+                            Log.d(TAG, "Signup error$er")
+                        } else {
+                            _txtMessage.text = "はじめまして"
+                            _txtLogin.text = "１回目ログイン、ありがとうございます。"
+                            //lastLoginを更新します（ラストログインのタイミングを取得するために）
+                            try {
+                                val curUser = NCMBUser.currentuser
+                                val now = Date()
+                                curUser!!.put("lastLoginDate", now)
+                                curUser.save()
+                            } catch (e1: NCMBException) {
+                                e1.printStackTrace()
+                            }
 
+                        }
                     }
-                }
+
+                })
             }
         } else {
-            val curUser = NCMBUser.getCurrentUser()
+            runOnUiThread {
+                val curUser = NCMBUser.currentuser
+                val lastLogin = curUser!!.getDate("lastLoginDate")
+                val simpleDate = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+                val strDt = simpleDate.format(lastLogin)
+                _txtMessage.text = "お帰りなさい！"
+                _txtLogin.text = "最終ログインは：$strDt"
 
-            val lastLogin = curUser.getDate("lastLoginDate")
-            val simpleDate = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
-            val strDt = simpleDate.format(lastLogin)
-            _txtMessage.text = "お帰りなさい！"
-            _txtLogin.text = "最終ログインは：$strDt"
-
-            //lastLoginを更新します（ラストログインのタイミングを取得するために）
-            try {
-                val now = Date()
-                curUser.put("lastLoginDate", now)
-                curUser.save()
-            } catch (e1: NCMBException) {
-                e1.printStackTrace()
+                //lastLoginを更新します（ラストログインのタイミングを取得するために）
+                try {
+                    val now = Date()
+                    curUser.put("lastLoginDate", now)
+                    curUser.save()
+                } catch (e1: NCMBException) {
+                    e1.printStackTrace()
+                }
             }
-
         }
-    }
+    })
 } catch (e: NCMBException) {
     e.printStackTrace()
 }
--- app/src/main/java/mbaas/com/nifcloud/androidautologinapp/MainActivity.kt
+++ app/src/main/java/mbaas/com/nifcloud/androidautologinapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.widget.TextView
 import com.nifcloud.mbaas.core.NCMB
+import com.nifcloud.mbaas.core.NCMBCallback
 import com.nifcloud.mbaas.core.NCMBException
 import com.nifcloud.mbaas.core.NCMBUser
 import java.text.SimpleDateFormat
@@ -21,11 +22,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         //**************** APIキーの設定 **************
-        NCMB.initialize(this, "YOUR_APPLICATION_KEY", "YOUR_CLIENT_KEY")
+        NCMB.initialize(
+            this,
+            "YOUR_APPLICATION_KEY",
+            "YOUR_CLIENT_KEY"
+        )
 
         // UUIDを取得します
-        uuid = Secure.getString(applicationContext.contentResolver,
-                Secure.ANDROID_ID)
+        uuid = Secure.getString(
+            applicationContext.contentResolver,
+            Secure.ANDROID_ID
+        )
         Log.d(TAG, uuid)
 
         setContentView(R.layout.activity_main)
@@ -34,59 +41,57 @@ class MainActivity : AppCompatActivity() {
 
         //Login
         try {
-            NCMBUser.loginInBackground(uuid, uuid) { login_user, e ->
+            var user = NCMBUser()
+            user.loginInBackground(uuid, uuid, NCMBCallback { e, login_user ->
                 if (e != null) {
                     //エラー時の処理
                     Log.d(TAG, e.code)
                     if (e.code == "E401002") {
-
                         //NCMBUserのインスタンスを作成
-                        val user = NCMBUser()
-                        //ユーザ名を設定
-                        user.userName = uuid
-                        //パスワードを設定
-                        user.setPassword(uuid)
-                        //設定したユーザ名とパスワードで会員登録を行う
-                        user.signUpInBackground { er ->
-                            if (er != null) {
-                                //会員登録時にエラーが発生した場合の処理
-                                Log.d(TAG, "Signup error$er")
-                            } else {
-                                _txtMessage.text = "はじめまして"
-                                _txtLogin.text = "１回目ログイン、ありがとうございます。"
-                                //lastLoginを更新します（ラストログインのタイミングを取得するために）
-                                try {
-                                    val curUser = NCMBUser.getCurrentUser()
-                                    val now = Date()
-                                    curUser.put("lastLoginDate", now)
-                                    curUser.save()
-                                } catch (e1: NCMBException) {
-                                    e1.printStackTrace()
-                                }
+                        val signupUser = NCMBUser()
+                        signupUser.signUpInBackground(uuid, uuid, NCMBCallback { er, signUpUser ->
+                            runOnUiThread {
+                                if (er != null) {
+                                    //会員登録時にエラーが発生した場合の処理
+                                    Log.d(TAG, "Signup error$er")
+                                } else {
+                                    _txtMessage.text = "はじめまして"
+                                    _txtLogin.text = "１回目ログイン、ありがとうございます。"
+                                    //lastLoginを更新します（ラストログインのタイミングを取得するために）
+                                    try {
+                                        val curUser = NCMBUser.currentuser
+                                        val now = Date()
+                                        curUser!!.put("lastLoginDate", now)
+                                        curUser.save()
+                                    } catch (e1: NCMBException) {
+                                        e1.printStackTrace()
+                                    }
 
+                                }
                             }
-                        }
+
+                        })
                     }
                 } else {
-                    val curUser = NCMBUser.getCurrentUser()
+                    runOnUiThread {
+                        val curUser = NCMBUser.currentuser
+                        val lastLogin = curUser!!.getDate("lastLoginDate")
+                        val simpleDate = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+                        val strDt = simpleDate.format(lastLogin)
+                        _txtMessage.text = "お帰りなさい！"
+                        _txtLogin.text = "最終ログインは：$strDt"
 
-                    val lastLogin = curUser.getDate("lastLoginDate")
-                    val simpleDate = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
-                    val strDt = simpleDate.format(lastLogin)
-                    _txtMessage.text = "お帰りなさい！"
-                    _txtLogin.text = "最終ログインは：$strDt"
-
-                    //lastLoginを更新します（ラストログインのタイミングを取得するために）
-                    try {
-                        val now = Date()
-                        curUser.put("lastLoginDate", now)
-                        curUser.save()
-                    } catch (e1: NCMBException) {
-                        e1.printStackTrace()
+                        //lastLoginを更新します（ラストログインのタイミングを取得するために）
+                        try {
+                            val now = Date()
+                            curUser.put("lastLoginDate", now)
+                            curUser.save()
+                        } catch (e1: NCMBException) {
+                            e1.printStackTrace()
+                        }
                     }
-
                 }
-            }
+            })
         } catch (e: NCMBException) {
             e.printStackTrace()
         }
